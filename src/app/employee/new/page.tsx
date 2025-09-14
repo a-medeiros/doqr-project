12f147d1099d0bac71eabf1b5e9f8c86988cbdbf
Ana Lúcia <ana.medeiros0097@gmail.com>
2025-09-14 11:53:47 -0300

    feat: create new employee page

---
+++ src/app/employee/new/page.tsx
@@ -0,0 +1,21 @@
+import { ArrowLeft } from 'lucide-react'
+import Link from 'next/link'
+
+const NewEmployeePage = () => {
+  return (
+    <div className="px-[130px] pt-8 pb-8 gap-16">
+      <div className="flex mb-4">
+        <Link href="/" className="flex items-center gap-2 w-[70px]">
+          <ArrowLeft size={14} />
+          Voltar
+        </Link>
+      </div>
+      <div className="flex flex-col gap-2 mb-8">
+        <h1 className="text-4xl font-bold text-black">Editar Funcionário</h1>
+        <p className="text-xl font-bold text-[#0B0B0C]">Empresa DoQR Tecnologia</p>
+      </div>
+    </div>
+  )
+}
+
+export default NewEmployeePage
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,21 @@
 import EmployeesList from '@/components/Employees/List'
+import { Button } from '@/components/ui/button'
+import { Plus } from 'lucide-react'
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] min-h-screen px-[130px] pt-8 gap-16">
-      <div className="flex flex-col gap-2">
+    <div className=" px-[130px] pt-8 pb-8 gap-16">
+      <div className="flex flex-col gap-2 mb-8">
         <h1 className="text-4xl font-bold text-black">Controle de Funcionários</h1>
         <p className="text-xl font-bold text-[#0B0B0C]">Empresa DoQR Tecnologia</p>
       </div>
 
+      <div className="mb-3">
+        <Button className="gap-[2.5px] cursor-pointer">
+          <Plus className="w-4 h-4" size={14} />
+          Novo Funcionário
+        </Button>
+      </div>
       <EmployeesList />
     </div>
   )
